6d48ad4db1b4c1206557c16ba4fc3203aa3fe648
Lumikins <krassparis@gmail.com>
2023-04-28 11:08:31 +0200

    :gem: add auth UI, register modal

---
+++ app/hooks/useRegisterModal.ts
@@ -0,0 +1,17 @@
+import { create } from "zustand";
+
+// create a store for dealing with the user registration modal
+
+interface RegisterModalStore {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+}
+
+const useRegisterModal = create<RegisterModalStore>((set) => ({
+  isOpen: false,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
+}));
+
+export default useRegisterModal;
